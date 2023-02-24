17497a1f80be1ad23319a6592acb647cd5e78e67
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-02-24 17:18:42 -0300

    :sparkles: feat: create settings and develop the main idea

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
-from rich import print
+import httpx
+from rich import print, print_json
+from rich.panel import Panel
+
+from settings import settings
+
 
 def main():
-  print('[b yellow] Developing...')
+  print('[b green][>-<] HIII!')
+  print('[>-<] Searching for the current value of the dolar...')
+
+  data = httpx.get(f'{settings.API_URL}/usd-brl,eur-brl').json()
+
+  print_json(data=data['USDBRL'])
+  print_json(data=data['EURBRL'])
+
 
 if __name__ == '__main__':
   main()
---
+++ src/settings.py
@@ -0,0 +1,4 @@
+class Settings:
+  API_URL: str = 'https://economia.awesomeapi.com.br/last' # USD-BRL, EUR-BRL, BTC-BRL
+
+settings = Settings()
