5b00f377f8d831c13cd14369b71fad929a9feb58
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-03-07 23:01:07 -0300

    :recycle: chore: add icon and write it to the output

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 import httpx
+from enum import Enum
 from rich import print, print_json
 from rich.panel import Panel
 
 from settings import settings
 
 
+class Icon(Enum):
+  PLUS = '[green][[/green]+[green]][/green]'
+  MINUS = '[red][[/red]-[red]][/red]'
+  INTERROGATIVE = '[yellow][[/yellow]?[yellow]][/yellow]'
+
+
 def main():
-  print('[b green][>-<] HIII!')
-  print('[>-<] Searching for the current value of the dolar...')
+  print(f'[b green]{Icon.PLUS.value} Welcome!')
+  print(f'{Icon.INTERROGATIVE.value} Searching for the current value of the dolar...')
 
   data = httpx.get(f'{settings.API_URL}/usd-brl,eur-brl').json()
 
